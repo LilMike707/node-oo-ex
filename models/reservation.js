1e25e8d5c18c34c5e4f167425a12d5cb7567a0c3
LilMike707 <michaelmarracq@yahoo.com>
2023-07-25 18:56:26 -0700

    done

---
+++ models/reservation.js
@@ -0,0 +1,57 @@
+/** Reservation for Lunchly */
+
+const moment = require("moment");
+
+const db = require("../db");
+
+/** A reservation for a party */
+
+class Reservation {
+  constructor({ id, customerId, numGuests, startAt, notes }) {
+    this.id = id;
+    this.customerId = customerId;
+    this.numGuests = numGuests;
+    this.startAt = startAt;
+    this.notes = notes;
+  }
+
+  /** formatter for startAt */
+
+  getformattedStartAt() {
+    return moment(this.startAt).format("MMMM Do YYYY, h:mm a");
+  }
+
+  /** given a customer id, find their reservations. */
+
+  static async getReservationsForCustomer(customerId) {
+    const results = await db.query(
+      `SELECT id, 
+           customer_id AS "customerId", 
+           num_guests AS "numGuests", 
+           start_at AS "startAt", 
+           notes AS "notes"
+         FROM reservations 
+         WHERE customer_id = $1`,
+      [customerId]
+    );
+
+    return results.rows.map((row) => new Reservation(row));
+  }
+
+  async save() {
+    if (this.id === undefined) {
+      const result = await db.query(
+        "insert into reservations (customer_id, num_guests, start_at, notes) values ($1, $2, $3, $4) returning id",
+        [this.customerId, this.numGuests, this.startAt, this.notes]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      await db.query(
+        "update reservations set num_guests=$1, start_at=$2, notes=$3 where id=$4",
+        [this.numGuests, this.startAt, this.notes, this.id]
+      );
+    }
+  }
+}
+
+module.exports = Reservation;
